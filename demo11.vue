5d9308de03a7c21a9a0c6739f5075f4250548428
huruqing <40466911@qq.com>
2017-10-15 15:03:05 +0800

    update demo11

---
+++ demo11.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="wrap">
+		<h3>单文件组件</h3>
+		<p>{{msg}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			msg: 'hello world'
+		}
+	}
+}
+</script>
+
+<style lang="less" scoped>
+.wrap {
+	border: 1px solid;
+	height: 100px;
+	h3 {
+		color: red;
+	}
+	p {
+		background: gray;
+	}
+}
+</style>
